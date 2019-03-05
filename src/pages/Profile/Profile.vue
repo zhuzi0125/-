c3a3bbadacfdd6d4578dabd228981c14a16c3176
zhuzi0125 <1356222047@qq.com>
2019-03-05 14:35:50 +0800

    first commit

---
+++ src/pages/Profile/Profile.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>ssssss</div>
+</template>
+<script>
+    export default{}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Msite from "../pages/Msite/Msite.vue"
+import Order from "../pages/Order/Order.vue"
+import Profile from "../pages/Profile/Profile.vue"
+import Search from "../pages/Search/Search.vue"
+//声明使用插件
+Vue.use(VueRouter)
+export default new VueRouter({
+  // 声明所有路由
+  routes:[
+    {
+      path:"/msite",
+      component:Msite
+    },
+    {
+      path:"/order",
+      component:Order
+    },
+    {
+      path:"/profile",
+      component:Profile
+    },
+    {
+      path:"/search",
+      component:Search
+    },
+    {
+      path:"/",
+      redirect:"/msite"
+    }
+
+  ]
+})
+
+
